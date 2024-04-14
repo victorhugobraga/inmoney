fa76c7de69a6ffb6806f16da858db9e301b761d5
Victor <victorhugo.br2013@gmail.com>
2024-04-14 03:41:07 -0300

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function Login() {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center mb-4">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Digite o seu email para entrar na sua conta
+            </p>
+          </div>
+
+          <div className="grid gap-8">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Senha</Label>
+                <Link
+                  href="/forgot-password"
+                  className="ml-auto inline-block text-xs underline"
+                >
+                  Esqueceu sua senha?
+                </Link>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+            {/* <Button variant="outline" className="w-full">
+              Login with Google
+            </Button> */}
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Não tem uma conta?{" "}
+            <Link href="#" className="underline">
+              Cadastre-se
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/placeholder.svg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
