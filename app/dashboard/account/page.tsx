2266deb63699032a577d91a7dd04ce2dab1e5521
Victor <victorhugo.br2013@gmail.com>
2024-04-16 20:00:36 -0300

    feat: edit account page

--- app/dashboard/account/page.tsx
+++ app/dashboard/account/page.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -8,75 +9,61 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import Link from "next/link";
+import { Label } from "@/components/ui/label";
 
 export default function Account() {
   return (
-    <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-      <div className="mx-auto grid w-full max-w-6xl gap-2">
-        <h1 className="text-3xl font-semibold">Settings</h1>
-      </div>
-      <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-        <nav
-          className="grid gap-4 text-sm text-muted-foreground"
-          x-chunk="dashboard-04-chunk-0"
-        >
-          <Link href="#" className="font-semibold text-primary">
-            General
-          </Link>
-          <Link href="#">Security</Link>
-          <Link href="#">Integrations</Link>
-          <Link href="#">Support</Link>
-          <Link href="#">Organizations</Link>
-          <Link href="#">Advanced</Link>
-        </nav>
-        <div className="grid gap-6">
-          <Card x-chunk="dashboard-04-chunk-1">
-            <CardHeader>
-              <CardTitle>Store Name</CardTitle>
-              <CardDescription>
-                Used to identify your store in the marketplace.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form>
-                <Input placeholder="Store Name" />
-              </form>
-            </CardContent>
-            <CardFooter className="border-t px-6 py-4">
-              <Button>Save</Button>
-            </CardFooter>
-          </Card>
-          <Card x-chunk="dashboard-04-chunk-2">
-            <CardHeader>
-              <CardTitle>Plugins Directory</CardTitle>
-              <CardDescription>
-                The directory within your project, in which your plugins are
-                located.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form className="flex flex-col gap-4">
-                <Input
-                  placeholder="Project Name"
-                  defaultValue="/content/plugins"
-                />
-                <div className="flex items-center space-x-2">
-                  {/* <CheckboxItem id="include" defaultChecked /> */}
-                  <label
-                    htmlFor="include"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Allow administrators to change the directory.
-                  </label>
-                </div>
-              </form>
-            </CardContent>
-            <CardFooter className="border-t px-6 py-4">
-              <Button>Save</Button>
-            </CardFooter>
-          </Card>
-        </div>
+    <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap0-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+      <div className="mx-auto grid w-full max-w-7xl gap-2 items-start md:grid-cols-[180px_1fr] lg:grid-cols-[1fr_2fr]">
+        <Card>
+          <CardHeader>
+            <CardTitle>Perfil</CardTitle>
+            <CardDescription>Suas informações de perfil.</CardDescription>
+          </CardHeader>
+          <CardContent className="flex gap-4 items-center">
+            <Avatar>
+              <AvatarImage src="https://github.com/victorhugobraga.png" />
+              <AvatarFallback>IM</AvatarFallback>
+            </Avatar>
+            <div className="space-y-2.5">
+              <div className="flex items-center space-x-2">
+                <h1 className="text-2xl font-bold leading-none">
+                  Victor Braga
+                </h1>
+              </div>
+              <p className="text-sm text-gray-500">@victorbraga</p>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button variant="outline">Editar</Button>
+          </CardFooter>
+        </Card>
+
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Editar Perfil</CardTitle>
+            <CardDescription>
+              Atualização de informações de seu perfil.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="name">Nome</Label>
+              <Input id="name" placeholder="Digite o seu nome" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" placeholder="Digite o seu email" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="avatar">Avatar</Label>
+              <Input id="avatar" type="file" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="ml-auto">Save</Button>
+          </CardFooter>
+        </Card>
       </div>
     </main>
   );
