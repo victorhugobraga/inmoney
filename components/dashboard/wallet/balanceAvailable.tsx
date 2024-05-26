2725fd6a2567091b8a06f33978d232f95b5c3e67
Victor <victorhugo.br2013@gmail.com>
2024-05-26 02:08:03 -0300

    feat(wallet): add modal submit transactions

--- components/dashboard/wallet/balanceAvailable.tsx
+++ components/dashboard/wallet/balanceAvailable.tsx
@@ -17,7 +17,14 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ArrowDownSquare, ArrowUpSquare, Repeat } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ArrowDownSquare, ArrowUpSquare } from "lucide-react";
 
 export default function BalanceAvailable() {
   return (
@@ -35,46 +42,141 @@ export default function BalanceAvailable() {
         </div>
       </CardContent>
 
-      <CardFooter className="grid gap-3 grid-cols-3">
+      <CardFooter className="gap-3 flex">
         <Dialog>
           <DialogTrigger asChild>
-            <Button variant="outline" className="w-full">
+            <Button variant="outline" className="flex-1">
               <ArrowUpSquare className="w-5 h-5 mr-2" />
               Receita
             </Button>
           </DialogTrigger>
           <DialogContent>
             <DialogHeader className="space-y-4">
-              <DialogTitle>Cadastrar Receita</DialogTitle>
+              <DialogTitle className="flex items-center gap-1">
+                <ArrowUpSquare className="w-5 h-5 mr-2" /> Cadastrar Receita
+              </DialogTitle>
             </DialogHeader>
             <DialogDescription className="space-y-8">
-              <div className="space-y-2">
-                <Label htmlFor="name">Nome</Label>
-                <Input id="name" placeholder="Digite o seu nome" />
+              <div className="space-y-1 flex items-baseline space-x-2">
+                <span className="font-bold">R$</span>
+                <Input className="rounded-none border-x-0 border-t-0 focus-visible:border-green-500 focus-visible:ring-0 p-1 pb-0 text-xl font-bold" />
               </div>
-              <div className="space-y-2">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" placeholder="Digite o seu email" />
+              <div className="space-y-1">
+                <Label htmlFor="email">Descrição</Label>
+                <Input placeholder="" />
               </div>
-              <div className="space-y-2">
-                <Label htmlFor="avatar">Avatar</Label>
-                <Input id="avatar" type="file" />
+
+              <div className="space-y-1">
+                <Label htmlFor="email">Banco</Label>
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Selecione o banco" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="nenhum">Nenhum</SelectItem>
+                    <SelectItem value="nubank">NuBank</SelectItem>
+                    <SelectItem value="inter">Inter</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+
+              <div className="flex gap-4">
+                <div className="flex-1 space-y-1">
+                  <Label htmlFor="email">Data</Label>
+                  <Input type="date" />
+                </div>
+                <div className="flex-1 space-y-1">
+                  <Label htmlFor="email">Categoria</Label>
+                  <Select>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Selecione" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="lazer">Lazer</SelectItem>
+                      <SelectItem value="comida">Comida</SelectItem>
+                      <SelectItem value="uber">Uber</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
               </div>
             </DialogDescription>
             <DialogFooter>
               <Button>Salvar</Button>
+              <Button variant="outline">Cancelar</Button>
             </DialogFooter>
           </DialogContent>
         </Dialog>
 
-        <Button variant="outline">
-          <ArrowDownSquare className="w-5 h-5 mr-2" />
-          Despesa
-        </Button>
-        <Button variant="outline">
-          <Repeat className="w-5 h-5 mr-2" />
-          Transferência
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" className="flex-1">
+              <ArrowDownSquare className="w-5 h-5 mr-2" />
+              Despesa
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader className="space-y-4">
+              <DialogTitle className="flex items-center gap-1">
+                <ArrowDownSquare className="w-5 h-5 mr-2" /> Cadastrar Despesa
+              </DialogTitle>
+            </DialogHeader>
+            <DialogDescription className="space-y-8">
+              <div className="space-y-1 flex items-baseline space-x-2">
+                <span className="font-bold">R$</span>
+                <Input className="rounded-none border-x-0 border-t-0 focus-visible:border-red-500 focus-visible:ring-0 p-1 pb-0 text-xl font-bold" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="email">Descrição</Label>
+                <Input placeholder="" />
+              </div>
+
+              <div className="flex gap-4">
+                <div className="space-y-1 flex-1">
+                  <Label htmlFor="email">Banco</Label>
+                  <Select>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Selecione o banco" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="nenhum">Nenhum</SelectItem>
+                      <SelectItem value="nubank">NuBank</SelectItem>
+                      <SelectItem value="inter">Inter</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+
+                <div className="space-y-1 flex-1">
+                  <Label htmlFor="email">Parcelas</Label>
+                  <Input type="number" />
+                </div>
+              </div>
+
+              <div className="flex gap-4">
+                <div className="flex-1 space-y-1">
+                  <Label htmlFor="email">Data</Label>
+                  <Input type="date" />
+                </div>
+                <div className="flex-1 space-y-1">
+                  <Label htmlFor="email">Categoria</Label>
+                  <Select>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Selecione" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="lazer">Lazer</SelectItem>
+                      <SelectItem value="comida">Comida</SelectItem>
+                      <SelectItem value="uber">Uber</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </DialogDescription>
+            <DialogFooter>
+              <Button>Salvar</Button>
+              <Button variant="outline">Cancelar</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </CardFooter>
     </Card>
   );
